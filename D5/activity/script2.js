39e13f65dfacdfe8b39e2dfc4e9e980bb0ea6f51
ejandrea <ejcanatoy@protonmail.com>
2024-11-08 21:54:25 +0800

    Finished Activity 2: Day 5

---
+++ D5/activity/script2.js
@@ -0,0 +1,7 @@
+const startingRange = parseInt(prompt("Enter starting range: "));
+
+const endingRange = parseInt(prompt("Enter ending range: "));
+
+for (let i = startingRange; i <= endingRange; i++) {
+  console.log(i);
+}
